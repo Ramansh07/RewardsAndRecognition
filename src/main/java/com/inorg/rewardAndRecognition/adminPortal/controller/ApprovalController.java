f1fda57d7424190cdde24f0be02bb7d70662b122
zoro <ramanshgoel@gmail.com>
2024-07-10 10:24:28 +0530

    changed the uri's according to rest api's

--- src/main/java/com/inorg/rewardAndRecognition/adminPortal/controller/ApprovalController.java
+++ src/main/java/com/inorg/rewardAndRecognition/adminPortal/controller/ApprovalController.java
@@ -3,17 +3,12 @@ import com.inorg.rewardAndRecognition.adminPortal.DTO.ApprovalUpdateDTO;
 import com.inorg.rewardAndRecognition.adminPortal.DTO.PendingApprovalsDTO;
 import com.inorg.rewardAndRecognition.adminPortal.service.ApprovalService;
 import com.inorg.rewardAndRecognition.common.DTO.ResponseDTO;
-import com.inorg.rewardAndRecognition.common.DTO.RewardDTO;
-import com.inorg.rewardAndRecognition.common.exceptions.ResourceNotFoundException;
-import com.inorg.rewardAndRecognition.userPortal.dto.NominatorHistoryDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/admin-portal")
@@ -26,8 +21,8 @@ public class ApprovalController {
         this.approvalService = approvalService;
     }
 
-    @GetMapping("/status-approvals/{userId}/{approvalLevel}/{approvalStatus}")
-    public ResponseEntity<ResponseDTO> getApprovalStatus(@PathVariable String userId, @PathVariable int approvalLevel, @PathVariable int approvalStatus) throws Exception{
+    @GetMapping("/nomination-status")
+    public ResponseEntity<ResponseDTO> getApprovalStatus(@RequestParam String userId, @RequestParam int approvalLevel, @RequestParam int approvalStatus) throws Exception{
         List<PendingApprovalsDTO> pendingApprovals = approvalService.getApprovalStatus(userId, approvalLevel, approvalStatus);
 
         ResponseDTO response = ResponseDTO.build(
@@ -41,8 +36,8 @@ public class ApprovalController {
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
-    @PutMapping("/status-change/{userId}")
-    public ResponseEntity<ResponseDTO> statusChange(@RequestBody List<ApprovalUpdateDTO> dto, @PathVariable String userId) throws Exception{
+    @PutMapping("employee/change-status")
+    public ResponseEntity<ResponseDTO> statusChange(@RequestBody List<ApprovalUpdateDTO> dto, @RequestParam String userId) throws Exception{
 
 
         ResponseDTO response = ResponseDTO.build(
@@ -56,7 +51,7 @@ public class ApprovalController {
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
-    @GetMapping("/approval-status")
+    @GetMapping("employee/approval-status")
     public ResponseEntity<ResponseDTO> getNominatorHistory(@RequestParam String nominatorId) throws Exception{
 
         return ResponseEntity.ok().body(ResponseDTO.build(
--- src/main/java/com/inorg/rewardAndRecognition/adminPortal/controller/RewardController.java
+++ src/main/java/com/inorg/rewardAndRecognition/adminPortal/controller/RewardController.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class RewardController {
 
     private final RewardService rewardService;
+    int a;
 
     @Autowired
     public RewardController(RewardService rewardService) {
